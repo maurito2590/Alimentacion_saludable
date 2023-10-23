2522aa33fb0dc30238743caca91a808b723d92a0
unknown <you@example.com>
2023-10-23 14:28:05 -0300

    miprimercommitdecontactos

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alimentacion Saludable
+ Proyecto fullstack tienda de alimentacion saludable
+
+
+Empezamos no sabiendo bien hacia donde se iba a dirigir nuestro proyecto, por eso decidimos pensar un poco mas alla y buscar algun tema que sea de actualidad y que ademas nos permita mantenerlo en el tiempo para poder no solo presentarlo como proyecto sino tambien que forme parte del portfolio.
---
+++ js/main.js
@@ -0,0 +1,55 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>
+{
+    searchForm.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>
+{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>
+{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>
+{   
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+const btns = document.querySelectorAll(".my-form button");
+
+checkbox.addEventListener("change", function() {
+  const checked = this.checked;
+  for (const btn of btns) {
+    checked ? (btn.disabled = false) : (btn.disabled = true);
+  }
+});
+function validateForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const tel = document.getElementById("tel").value;
+  const sede = document.querySelector(".select").value;
+  const consult = document.getElementById("consult").value;
+  const warnings = document.getElementById("warnings");
+if (name === "" || email === "" || tel === "" || sede === "Elige tu sede" || consult === "") {
+      warnings.innerHTML = "Por favor, complete todos los campos";
+      return false;
+  }
+}
